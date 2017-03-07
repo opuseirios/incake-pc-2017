e02e959b0e71a7db10e5113d879d74d39e051257
armdong <armdong@163.com>
2017-03-07 21:01:00 +0800

    头部布局基本完成

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,44 @@
 (function(window, $, undefined) {
 	var doc = window.document;
 	$(doc).on('ready', function() {
+		// 导航栏
+		fnInitHeaderNav();
+		// 轮播图
 		fnInitMainSlider();
 	});
+
+	function fnInitHeaderNav() {
+		var $oNav = $('#menuNav'),
+			$oIndicator = $oNav.find('.indicator'),
+			$oActive = null,
+			tl = new TimelineMax(),
+			iNavL = $oNav.offset().left,
+			iLeft = 0;
+
+		$oActive = $oNav.find('.active');
+		$oIndicator.css({
+			left: $oActive.offset().left - iNavL + 'px'
+		});
+
+		$oNav.on('mouseover click', 'a', function() {
+			iLeft = $(this).offset().left;
+			tl.clear();
+			tl.to($oIndicator, 0.3, {
+				left: iLeft - iNavL + 'px',
+				ease: Back.easeOut
+			});
+		}).on('mouseleave', 'a', function() {
+			iLeft = $oActive.offset().left;
+			tl.clear();
+			tl.to($oIndicator, 0.3, {
+				left: iLeft - iNavL + 'px',
+				ease: Back.easeOut
+			}, 0.15);
+		}).on('click', 'a', function() {
+			$oActive = $(this);
+			$(this).addClass('active').siblings('a').removeClass('active');
+		});
+	}
 
 	function fnInitMainSlider() {
 		var swiper = new Swiper('#mainSlider .swiper-container', {
