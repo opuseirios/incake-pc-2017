fa7408784df937f758cf54986d369b56e855cc60
armdong <armdong@163.com>
2017-03-08 15:58:24 +0800

    首页当季热卖模块完成

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,11 +3,143 @@
 	$(doc).on('ready', function() {
 		// 轮播图
 		fnInitMainSlider();
+
+		// 绑定当季热卖数据
+		fnBindBestChoice();
 	});
 
+	function fnBindBestChoice() {
+		var $oBestChoice = $('#bestChoice');
+		var _data = {
+			list: [{
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_01.jpg',
+				favor: false,
+				name: {
+					cn: '小鸡派对',
+					en: 'Chick Party'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_02.jpg',
+				favor: true,
+				name: {
+					cn: '百变魔方',
+					en: 'Rubik Cube'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_03.jpg',
+				favor: false,
+				name: {
+					cn: '小鸡派对',
+					en: 'Chick Party'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_04.jpg',
+				favor: false,
+				name: {
+					cn: '百变魔方',
+					en: 'Rubik Cube'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_01.jpg',
+				favor: false,
+				name: {
+					cn: '小鸡派对',
+					en: 'Chick Party'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_02.jpg',
+				favor: false,
+				name: {
+					cn: '百变魔方',
+					en: 'Rubik Cube'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_03.jpg',
+				favor: false,
+				name: {
+					cn: '小鸡派对',
+					en: 'Chick Party'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_04.jpg',
+				favor: false,
+				name: {
+					cn: '百变魔方',
+					en: 'Rubik Cube'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_01.jpg',
+				favor: false,
+				name: {
+					cn: '小鸡派对',
+					en: 'Chick Party'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_02.jpg',
+				favor: false,
+				name: {
+					cn: '百变魔方',
+					en: 'Rubik Cube'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_03.jpg',
+				favor: false,
+				name: {
+					cn: '小鸡派对',
+					en: 'Chick Party'
+				},
+				price: 198,
+				pound: 1.5
+			}, {
+				link: 'javascript:;',
+				imgUrl: 'assets/imgs/list/cake_04.jpg',
+				favor: false,
+				name: {
+					cn: '百变魔方',
+					en: 'Rubik Cube'
+				},
+				price: 198,
+				pound: 1.5
+			}]
+		};
+		var _html = template('tplBestChoice', _data);
+		$oBestChoice.html(_html);
+	}
+
 	function fnInitMainSlider() {
-		var $oLayoutContainer = $('#layoutContainer'),
-			$oMainSlider = $oLayoutContainer.find('.main-slider'),
+		var $oHomePage = $('#homePage'),
+			$oMainSlider = $oHomePage.find('.main-slider'),
 			$oMainSwiper = $oMainSlider.find('.swiper-container'),
 			_swiper = null,
 			count = $oMainSlider.find('.swiper-slide').length;
