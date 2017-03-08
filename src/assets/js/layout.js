001064bb0a62dfef47679f8cdde6affe7fb85aea
armdong <armdong@163.com>
2017-03-08 18:02:28 +0800

    导航栏购物篮完成

--- src/assets/js/layout.js
+++ src/assets/js/layout.js
@@ -6,11 +6,73 @@
 		fnInitHeaderNav();
 		// 城市切换
 		fnInitCitySwitch();
+		// 购物篮
+		fnInitBasket();
 
 		$(window).on('resize', function() {
 			fnInitHeaderNav();
 		});
 	});
+
+	function fnInitBasket() {
+
+		var $oBasket = $('#layoutHeader').find('.basket'),
+			$oView = $oBasket.find('.view'),
+			$oArrow = $oBasket.find('.arrow'),
+			$oContent = $oBasket.find('.content'),
+			isExpand = false;
+
+		$oView.on('click', function() {
+			if(!isExpand) {
+				$oArrow.addClass('active');
+				$oContent.slideDown();
+			} else {
+				$oArrow.removeClass('active');
+				$oContent.slideUp();
+			}
+			isExpand = !isExpand;
+		});
+
+		// 绑定购物车数据
+		fnBindBasket();
+	}
+
+	function fnBindBasket() {
+		var $oBasketList = $('#layoutHeader').find('.basket-list');
+		var _data = {
+			list: [{
+				link: 'javascript:;',
+				thumbUrl: 'assets/imgs/list/cake_thumb_01.jpg',
+				name: '百变魔方',
+				price: 198,
+				pound: 1.5,
+				num: 1
+			}, {
+				link: 'javascript:;',
+				thumbUrl: 'assets/imgs/list/cake_thumb_01.jpg',
+				name: '百变魔方',
+				price: 198,
+				pound: 1.5,
+				num: 1
+			}, {
+				link: 'javascript:;',
+				thumbUrl: 'assets/imgs/list/cake_thumb_01.jpg',
+				name: '百变魔方',
+				price: 198,
+				pound: 1.5,
+				num: 1
+			}, {
+				link: 'javascript:;',
+				thumbUrl: 'assets/imgs/list/cake_thumb_01.jpg',
+				name: '百变魔方',
+				price: 198,
+				pound: 1.5,
+				num: 1
+			}]
+		};
+		var _html = template('tplBasketList', _data);
+		$oBasketList.html(_html);
+	}
 
 	function fnInitCitySwitch() {
 		var $oLocator = $('#layoutHeader').find('.locator'),
