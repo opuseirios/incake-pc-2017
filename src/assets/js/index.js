33a5d42f62fcbebc24cbebd460d7150e1e98d02d
armdong <armdong@163.com>
2017-03-08 13:02:08 +0800

    首页轮播图完成

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -6,13 +6,45 @@
 	});
 
 	function fnInitMainSlider() {
-		var swiper = new Swiper('#mainSlider .swiper-container', {
-	        pagination: '.swiper-pagination',
-	        paginationClickable: true,
-	        loop: true,
-	        autoplay: 5000,
-	        autoplayDisableOnInteraction: false,
-	        speed: 1000
-	    });
+		var $oLayoutContainer = $('#layoutContainer'),
+			$oMainSlider = $oLayoutContainer.find('.main-slider'),
+			$oMainSwiper = $oMainSlider.find('.swiper-container'),
+			_swiper = null,
+			count = $oMainSlider.find('.swiper-slide').length;
+
+		if(count > 1) {
+			_swiper = $oMainSwiper.swiper({
+		        pagination: '.swiper-pagination',
+		        paginationClickable: true,
+		        loop: true,
+		        autoplay: 5000,
+		        autoplayDisableOnInteraction: false,
+		        speed: 1000
+		    });
+
+		    // slide prev
+		    $oMainSlider.on('click', '.prev-slide', function(e) {
+		    	e.preventDefault();
+		    	_swiper.swipePrev();
+		    });
+
+		    // slide next
+		    $oMainSlider.on('click', '.next-slide', function(e) {
+		    	e.preventDefault();
+		    	_swiper.swipeNext();
+		    });	
+
+		    $oMainSlider.hover(function() {
+		    	_swiper.stopAutoplay();
+		    }, function() {
+		    	_swiper.startAutoplay();
+		    });
+		} else {
+			_swiper = $oMainSwiper.swiper({
+				autoplay: false
+			});
+			$oMainSlider.find('.prev-slide').hide();
+			$oMainSlider.find('.next-slide').hide();
+		}
 	}
 })(window, jQuery);
