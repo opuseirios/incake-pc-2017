6feb77dffc2a50de7a1fc6cf0fc6462a7cee2c01
armdong <armdong@163.com>
2017-04-19 12:01:52 +0800

    详情页图片预览功能完善

--- src/assets/js/detail.js
+++ src/assets/js/detail.js
@@ -4,6 +4,7 @@
 		fnInitPreview();
 	});
 
+	// 初始化图片预览功能
 	function fnInitPreview() {
 		$('.jqzoom').jqzoom({
             zoomWidth: 540,
@@ -17,15 +18,72 @@
             hideEffect: 'fadeout'
         });
 
-		var $thumblist = $('#thumblist');
+		var $thumblist = $('#thumblist'),
+			$thumbli = $thumblist.children('li'),
+			$thumbs = $thumblist.closest('.thumbs'),
+			$btnPrev = $thumbs.find('.btn-prev'),
+			$btnNext = $thumbs.find('.btn-next');
 
-		// resize thumblist width
+		// init thumblist width
 		(function() {
 	        var totalW = 0;
 	        $thumblist.find('li').each(function(idx, ele) {
 	        	totalW += $(ele).width() + parseInt($(ele).css('margin-right'), 10);
 	        });
 	        $thumblist.width(totalW);
+        })();
+
+        // init thumbs
+        (function() {
+        	var listLen = $thumbli.length,
+        		currIndex = 0;
+        	$btnPrev.addClass('disabled');
+    		if(listLen <= 4) {
+    			$btnNext.addClass('disabled');
+    		} else if(listLen > 4) {
+    			currIndex = 3;
+    		}
+
+    		var liW = 110,
+    			liM = 10,
+    			tl = new TimelineLite(),
+    			totalW = listLen * liW + (listLen - 1) * liM,
+    			iLeft = 0;
+
+    		// handler for next thumb
+    		$btnNext.on('click', function(e) {
+    			if($(this).hasClass('disabled')) {
+    				return false;
+    			}
+    			$btnPrev.removeClass('disabled');
+    			currIndex ++;
+    			if(currIndex == listLen - 1) { // last item
+    				$(this).addClass('disabled');
+    			}	
+    			iLeft += liW + liM;
+    			tl.clear();
+				tl.to($thumblist, 0.5, {
+					x: -iLeft + 'px'
+				});
+    		});
+
+    		// handler for prev thumb
+    		$btnPrev.on('click', function(e) {
+    			if($(this).hasClass('disabled')) {
+    				return false;
+    			}
+    			$btnNext.removeClass('disabled');
+    			currIndex --;
+    			if(currIndex == 3) { // last item
+    				$(this).addClass('disabled');
+    			}
+    			iLeft -= liW + liM;	
+    			tl.clear();
+				tl.to($thumblist, 0.5, {
+					x: -iLeft + 'px'
+				});
+    		});
+
         })();
 	}
 
---
+++ src/assets/js/latestorders.js
@@ -0,0 +1,83 @@
+
+/**
+ * 对时间戳进行格式化
+ * @param  spec    要格式化的时间戳
+ * @param  format  进行格式化的规格字符串
+ * @return [description]
+ */
+template.helper('timestampFormat', function(timestamp) {
+    var time = '';
+    if(!!timestamp){
+    	if(typeof timestamp == 'string') {
+    		timestamp = parseInt(timestamp);
+    	}
+        time = moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+    }
+    return time;
+});
+
+;(function(window, $, undefined) {
+
+	$(function() {
+		fnInitList();
+	});
+
+	// 初始化列表数据
+	function fnInitList() {
+		var $oOrderList = $('#orderList');
+
+		var _data = {
+			list: [{
+				orderNo: 'SHW0865760',
+				pubTime: new Date().getTime().toString(),
+				deliveryType: 'incake快递',
+				currentState: '初始',
+				detailLink: 'javascript:;',
+				cakes: [{
+					link: '/detail.html',
+					thumbImg: '/assets/imgs/ucenter/cake_thumb.jpg',
+					name: '樱桃冻芝士',
+					pound: 1.5,
+					price: 189,
+					counts: 1,
+					require: '暂无内容'
+				}, {
+					link: '/detail.html',
+					thumbImg: '/assets/imgs/ucenter/cake_thumb.jpg',
+					name: '樱桃冻芝士',
+					pound: 1.5,
+					price: 189,
+					counts: 1,
+					require: '暂无内容'
+				}],
+				totalMoney: 189,
+				paymentType: '支付宝支付',
+				orderStatus: '<a href="javascript:;" class="cancel">取消订单</a><a href="javascript:;" class="payonline">在线支付</a><p>未支付</p>',
+				orderOperates: '<a href="javascript:;" class="rebuy">再购物</a>'
+			}, {
+				orderNo: 'SHW0865760',
+				pubTime: Date.now().toString(),
+				deliveryType: 'incake快递',
+				currentState: '初始',
+				detailLink: 'javascript:;',
+				cakes: [{
+					link: '/detail.html',
+					thumbImg: '/assets/imgs/ucenter/cake_thumb.jpg',
+					name: '樱桃冻芝士',
+					pound: 1.5,
+					price: 189,
+					counts: 1,
+					require: '暂无内容'
+				}],
+				totalMoney: 189,
+				paymentType: '支付宝支付',
+				orderStatus: '<a href="javascript:;" class="cancel">取消订单</a><a href="javascript:;" class="payonline">在线支付</a><p>未支付</p>',
+				orderOperates: '<a href="javascript:;" class="rebuy">再购物</a>'
+			}]
+		};
+
+		var _html = template('tplOrderList', _data);
+		$oOrderList.html(_html);
+	}
+
+})(window, jQuery);
