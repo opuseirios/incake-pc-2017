7c6c89e8afd2ccc8b010a44fa5f1b2397ed7a8ec
armdong <armdong@163.com>
2017-06-04 11:20:17 +0800

    修改公共弹窗样式

--- src/assets/js/popups.js
+++ src/assets/js/popups.js
@@ -14,38 +14,38 @@
 			$btns = $popupsContainer.find('.btns'),
 			$btnClick = $btns.find('.btn-click'),
 			$btnClick2 = $btns.find('.btn-click2'),
-			
-			$maskList = $popupsPage.find('#mask-list'),
+
+			$maskList = $popupsPage.find('#maskPopup'),
 			$popups =  $maskList.find('.popups'),
-			
+
 			$popResult = $maskList.find('.popup-result'),
 			$btnCancel = $popResult.find('.btn-cancel'),
-			
+
 			$popResult2 = $maskList.find('.popup-result2'),
 			$btnClose = $popResult2.find('.btn-close');
-    	
+
     	$btnClick.on('click', function(){
     		$maskList.fadeIn(200, function(){
     			$popResult.fadeIn();
     		});
     	});
-		
+
     	$btnCancel.on('click', function(){
     		$popResult.fadeOut();
     		$maskList.fadeOut();
 	    });
-    	
+
     	$btnClick2.on('click', function(){
     		$maskList.fadeIn(200, function(){
     			$popResult2.fadeIn();
     		});
     	});
-    	
+
     	$btnClose.on('click', function(){
     		$popResult2.fadeOut();
     		$maskList.fadeOut();
 	    });
-    	
+
     }
 
 })(window, document, jQuery);
