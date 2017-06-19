2a0a154d1e08b37662cf1047377d8e23ca3d7709
armdong <armdong@163.com>
2017-06-19 13:22:10 +0800

    add gift entry page

---
+++ src/assets/js/gift-entry.js
@@ -0,0 +1,100 @@
+(function(window, $, undefined) {
+
+  $(function() {
+
+    // 绑定蛋糕列表数据
+    fnBindCakeList();
+  });
+
+  function fnBindCakeList() {
+      var $oCakeList = $('#cakeList');
+      var _data = {
+          list: [
+              {
+                  link: 'detail.html',
+                  imgUrl: '/assets/imgs/list/cake_01.jpg',
+                  desc: '纯芝士与醇香奶油的梦幻组合',
+                  name: {
+                      cn: '小鸡派对',
+                      en: 'Chick Party'
+                  },
+                  price: 189,
+                  pound: 1.5
+              }, {
+                  link: 'detail.html',
+                  imgUrl: '/assets/imgs/list/cake_02.jpg',
+                  desc: '纯芝士与醇香奶油的梦幻组合',
+                  name: {
+                      cn: '百变魔方',
+                      en: 'Rubik Cube'
+                  },
+                  price: 189,
+                  pound: 1.5
+              }, {
+                  link: 'detail.html',
+                  imgUrl: '/assets/imgs/list/cake_03.jpg',
+                  desc: '纯芝士与醇香奶油的梦幻组合',
+                  name: {
+                      cn: '小鸡派对',
+                      en: 'Chick Party'
+                  },
+                  price: 189,
+                  pound: 1.5
+              }, {
+                  link: 'detail.html',
+                  imgUrl: '/assets/imgs/list/cake_04.jpg',
+                  desc: '纯芝士与醇香奶油的梦幻组合',
+                  name: {
+                      cn: '百变魔方',
+                      en: 'Rubik Cube'
+                  },
+                  price: 189,
+                  pound: 1.5
+              }, {
+                  link: 'detail.html',
+                  imgUrl: '/assets/imgs/list/cake_01.jpg',
+                  desc: '纯芝士与醇香奶油的梦幻组合',
+                  name: {
+                      cn: '小鸡派对',
+                      en: 'Chick Party'
+                  },
+                  price: 189,
+                  pound: 1.5
+              }, {
+                  link: 'detail.html',
+                  imgUrl: '/assets/imgs/list/cake_02.jpg',
+                  desc: '纯芝士与醇香奶油的梦幻组合',
+                  name: {
+                      cn: '百变魔方',
+                      en: 'Rubik Cube'
+                  },
+                  price: 189,
+                  pound: 1.5
+              }, {
+                  link: 'detail.html',
+                  imgUrl: '/assets/imgs/list/cake_03.jpg',
+                  desc: '纯芝士与醇香奶油的梦幻组合',
+                  name: {
+                      cn: '小鸡派对',
+                      en: 'Chick Party'
+                  },
+                  price: 189,
+                  pound: 1.5
+              }, {
+                  link: 'detail.html',
+                  imgUrl: '/assets/imgs/list/cake_04.jpg',
+                  desc: '纯芝士与醇香奶油的梦幻组合',
+                  name: {
+                      cn: '百变魔方',
+                      en: 'Rubik Cube'
+                  },
+                  price: 189,
+                  pound: 1.5
+              }
+          ]
+      };
+      var _html = template('tplCakeList', _data);
+      $oCakeList.html(_html);
+  }
+
+})(window, jQuery);
